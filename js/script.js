eb3710b60d279abcd2682ccb087d212980d7054c
anitkumarsingh <anikumarsingh99@gmail.com>
2022-08-12 18:02:47 +0530

    mobile menu toggling

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
 const yearEle = document.querySelector('.year');
 const currentYear = new Date().getFullYear();
-
 yearEle.textContent = currentYear;
+
+// Mobile menu working
+
+const mobileMenu = document.querySelector('.btn-mobile-nav');
+const headerEle = document.querySelector('.header');
+
+mobileMenu.addEventListener('click',function(){
+ headerEle.classList.toggle('nav-open');
+})
