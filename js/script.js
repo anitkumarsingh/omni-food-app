fcf5b52d7d2149ea7c2def6862d8f789060f040b
anitkumarsingh <anikumarsingh99@gmail.com>
2022-08-12 22:21:58 +0530

    sticky header with js

--- js/script.js
+++ js/script.js
@@ -17,21 +17,39 @@ allLink.forEach(function (link) {
 		e.preventDefault();
 		// get attribute name
 		const href = link.getAttribute('href');
-    // scroll back to top
+		// scroll back to top
 		if (href === '#') {
 			scrollTo({
 				top: 0,
 				behavior: 'smooth'
 			});
 		}
-    // scroll to section
-    if(href !=='#' && href.startsWith('#')){
-      const sectionEle = document.querySelector(href);
-      sectionEle.scrollIntoView({behavior:'smooth'})
-    }
-    // closing mobile menu
-    if(link.classList.contains('nav-main-link')){
-      headerEle.classList.toggle('nav-open')
-    }
+		// scroll to section
+		if (href !== '#' && href.startsWith('#')) {
+			const sectionEle = document.querySelector(href);
+			sectionEle.scrollIntoView({ behavior: 'smooth' });
+		}
+		// closing mobile menu
+		if (link.classList.contains('nav-main-link')) {
+			headerEle.classList.toggle('nav-open');
+		}
 	});
 });
+
+// ======= Sticky navigation ========
+const sectionHeroEle = document.querySelector('.hero-section');
+const obs = new IntersectionObserver(function (entries) {
+const ent = entries[0]
+if(!ent.isIntersecting){
+  document.body.classList.add('sticky');
+}else{
+  document.body.classList.remove('sticky');
+}
+}, {
+  // in view port
+	root: null,
+	threshold: 0,
+  // height of sticky header in px
+  rootMargin:'-80px'
+});
+obs.observe(sectionHeroEle);
