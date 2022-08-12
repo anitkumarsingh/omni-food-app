302064941df263a95b93def0b3f8f3a161bab06c
anitkumarsingh <anikumarsingh99@gmail.com>
2022-08-12 20:36:01 +0530

    smooth scrolling through js

--- js/script.js
+++ js/script.js
@@ -3,10 +3,35 @@ const currentYear = new Date().getFullYear();
 yearEle.textContent = currentYear;
 
 // Mobile menu working
-
 const mobileMenu = document.querySelector('.btn-mobile-nav');
 const headerEle = document.querySelector('.header');
 
-mobileMenu.addEventListener('click',function(){
- headerEle.classList.toggle('nav-open');
-})
+mobileMenu.addEventListener('click', function () {
+	headerEle.classList.toggle('nav-open');
+});
+
+// ======= Smooth scroll into section
+const allLink = document.querySelectorAll('a:link');
+allLink.forEach(function (link) {
+	link.addEventListener('click', function (e) {
+		e.preventDefault();
+		// get attribute name
+		const href = link.getAttribute('href');
+    // scroll back to top
+		if (href === '#') {
+			scrollTo({
+				top: 0,
+				behavior: 'smooth'
+			});
+		}
+    // scroll to section
+    if(href !=='#' && href.startsWith('#')){
+      const sectionEle = document.querySelector(href);
+      sectionEle.scrollIntoView({behavior:'smooth'})
+    }
+    // closing mobile menu
+    if(link.classList.contains('nav-main-link')){
+      headerEle.classList.toggle('nav-open')
+    }
+	});
+});
